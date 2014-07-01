47432e9f8e388f9361d7998bcbf12f2486cdc42a
Mike Perry <s14.aperry@gmail.com>
2014-06-30 20:33:20 -0400

    Initial Commit

---
+++ SimpleWeather/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SimpleWeather
+//
+//  Created by Mike Perry on 6/30/14.
+//  Copyright (c) 2014 Mike Perry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
